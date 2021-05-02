f636eb7a76da27b570d781e4ad702c30cee90406
danielufeli <danielufeli@yahoo.com>
2021-05-02 22:29:58 +0100

    Fix Linters

--- Gemfile
+++ Gemfile
@@ -33,10 +33,9 @@ gem 'devise', '~> 4.8'
 gem 'rubocop', '>= 1.0', '< 2.0'
 gem 'simple_form', '~> 5.1'
 
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'byebug', platforms: %I[mri mingw x64_mingw]
 end
 
 group :development do
@@ -56,4 +55,4 @@ group :test do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'tzinfo-data', platforms: %I[mingw mswin x64_mingw jruby]
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,16 +3,16 @@ class RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
     params.require(:user)
-          .permit(:name, :username, :email, :password, :password_confirmation)
+      .permit(:name, :username, :email, :password, :password_confirmation)
   end
 
   def account_update_params
     params.require(:user)
-          .permit(:name,
-                  :username,
-                  :email,
-                  :password,
-                  :password_confirmation,
-                  :current_password)
+      .permit(:name,
+              :username,
+              :email,
+              :password,
+              :password_confirmation,
+              :current_password)
   end
 end
